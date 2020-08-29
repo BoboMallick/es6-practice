e39ce35e1d47d78368a95e2eef158129f2824087
bobo mallick <mallickbobo@gmail.com>
2020-08-29 20:37:38 +0530

    default value done

---
+++ default.js
@@ -0,0 +1,19 @@
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+// const total = add(15, 17);
+// console.log(total);
+
+
+// function add(num1, num2){
+//         num2 = num2 || 20;
+//          return num1 + num2;
+//      }
+//      const total = add(15);
+//      console.log(total);
+
+     function add(num1, num2 = 20){
+         return num1 + num2;
+     }
+     const total = add(15);
+     console.log(total);
